f7e9aaabd94f1c8c711e5f75c7f7572be1dfdf2b
Sergey Karpov <srgk98@gmail.com>
2023-09-10 18:51:36 +0500

    Созданы модели Orders, Product

---
+++ Shop.Microservice.Domain/Common/Product.cs
@@ -0,0 +1,21 @@
+﻿
+
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Transactions;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Shop.Microservice.Domain.Common
+{
+    public class Product
+    {
+        [Key]
+        public Guid Id { get; set; }  
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Count { get; set; }
+        public int Price { get; set; }
+        public string Image { get; set; }
+
+    }
+}
