bba820c91bc90ec665680080871b8e02e925ba75
Nikita Glazkov <nik.glazkov2016@yandex.ru>
2023-09-14 18:47:43 +0300

    Модель Transaction

--- Shop.Microservice.Domain/Common/Transaction.cs
+++ Shop.Microservice.Domain/Common/Transaction.cs
@@ -8,16 +8,19 @@ using Authorization.Microservice.Domain.Entities;
 
 namespace Shop.Microservice.Domain.Common
 {
-    public class Order
+    public class Transaction
     {
         [Key]
         public Guid Id { get; set; }
 
         [ForeignKey("User")]
-        public Guid UserID { get; set; }
-        public List<Product> Producs { get; set; }
-        public OrderStatus OrderStatus { get; set; }
+        public Guid SenderID { get; set; }
 
+        public long Amount { get; set; }
+
+        public string Comment { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public TransactionStatus TransactionStatus { get; set; }
 
         public virtual User User { get; set; }
 
