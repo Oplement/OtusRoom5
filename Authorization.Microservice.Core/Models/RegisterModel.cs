2b732493ad911468179a239a553cfbcc232b564d
aglazev <andreiglazev@mail.ru>
2023-09-14 18:51:16 +0300

    Изменил местоположение моделей

---
+++ Authorization.Microservice.Core/Models/RegisterModel.cs
@@ -0,0 +1,17 @@
+﻿public class RegisterModel
+{
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Role { get; set; }
+
+
+
+    public RegisterModel(string username, string email, string password, string role)
+    {
+        Username = username;
+        Email = email;
+        Password = password;
+        Role = role;
+    }
+}
