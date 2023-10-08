0f5c46831d02d062b8a581c83858f5f2eff85199
aglazev <andreiglazev@mail.ru>
2023-10-08 13:29:36 +0300

    добавил хеширование пароля.

--- Authorization.Microservice.Infrastructure/Services/AuthService.cs
+++ Authorization.Microservice.Infrastructure/Services/AuthService.cs
@@ -1,5 +1,8 @@
 ﻿using Authorization.Microservice.Domain;
 using Authorization.Microservice.Infrastructure.Repositories.Contracts;
+using System.Runtime.ExceptionServices;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Authorization.Microservice.Infrastructure.Services
 {
@@ -25,6 +28,14 @@ namespace Authorization.Microservice.Infrastructure.Services
             return await _repository.Create(user);
         }
 
-
+        public async Task<string> Hash(string password)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                var hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                var hash = BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+                return hash;
+            }
+        }
     }
 }
