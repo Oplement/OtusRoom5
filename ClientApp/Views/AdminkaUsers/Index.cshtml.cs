c3226cf9491c2a1ad1edfd723327e85d104c4eec
Sergey Karpov <srgk98@gmail.com>
2024-01-29 23:49:29 +0500

    апр

---
+++ ClientApp/Views/AdminkaUsers/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ClientApp.Views.AdminkaUsers
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
