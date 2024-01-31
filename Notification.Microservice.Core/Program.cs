24693e75af0e9ff8aefc224fb96a1870c47dd182
Тим Гусь <guskov.tima@rambler.ru>
2024-01-31 13:21:46 +0300

    -+1 паттерн проектирования - доработка окна админки с заказами

--- Notification.Microservice.Core/Program.cs
+++ Notification.Microservice.Core/Program.cs
@@ -1,8 +1,15 @@
+using Notification.Microservice.Core.Interfaces;
 using Notification.Microservice.Core.Services;
+using Notification.Microservice.Core.Settings;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Добавление EmailService в контейнер служб
+builder.Services.AddScoped<IEmailService, EmailService>();
+
+// Добавление декоратора LoggingEmailServiceDecorator
+builder.Services.Decorate<IEmailService, LoggingEmailServiceDecorator>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
